fe4e3e6b5e5770ebd1d8f06c898c94a38ff98af7
raghavrajpoot88 <raghavrajpoot0115@gmail.com>
2025-06-06 11:25:45 +0530

    Adds the inventory methods

---
+++ Backend/InventorySystemAPI/DTOs/ContactCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventorySystemAPI.DTOs
+{
+    public class ContactCreateDto
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(50, ErrorMessage = "First name can not be longer than 50 characteres")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(100, ErrorMessage = "Last Name can not be longer than 100 characteres")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email address")]
+        public string? Email { get; set; }
+    }
+}
