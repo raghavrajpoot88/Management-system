cdfa92c56e074eab34ad5276e7d8a7d4ce230a36
raghavrajpoot88 <raghavrajpoot0115@gmail.com>
2025-06-27 10:38:44 +0530

    Adds the products category methods

---
+++ Backend/InventorySystemAPI/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using InventorySystemAPI.CustomActionFilters;
+using InventorySystemAPI.DTOs;
+using InventorySystemAPI.Models;
+using InventorySystemAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventorySystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoriesController : ControllerBase
+    {
+        public readonly IProductCategoryRepository _productCategoryRepository;
+
+        public ProductCategoriesController(IProductCategoryRepository productCategoryRepository)
+        {
+            _productCategoryRepository = productCategoryRepository;
+        }
+
+
+        // POST: api/ProductCategories
+        [HttpPost]
+        [ValidateModel]
+        public async Task<IActionResult> CreateProductCategory([FromBody] ProductCategoryCreateDto productCategoryDto)
+        {
+            try
+            {
+                var productCategory = new ProductCategory
+                {
+                    ProductCategoryName = productCategoryDto.ProductCategoryName
+                };
+
+                var newProductCategory = await _productCategoryRepository.CreateAsync(productCategory);
+                return CreatedAtAction(nameof(GetProductCategory), new { id = newProductCategory.Id }, newProductCategory);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
